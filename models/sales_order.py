6d2749d73daaffb524f11b14fdbb8d8f3ec9f14d
t-900-a <t-900@tutanota.com>
2020-12-06 17:32:45 -0500

    black & isort ran

--- models/sales_order.py
+++ models/sales_order.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
-import requests
-from odoo import models, fields, api
 import logging
+
+import requests
+from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
 
+
 class MoneroSalesOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     def _get_transaction_id(self):
         for record in self:
-            payment = self.env['payment.transaction'].search([('sale_order_id', '=', record.id)], limit=1)
+            payment = self.env["payment.transaction"].search(
+                [("sale_order_id", "=", record.id)], limit=1
+            )
             if payment:
                 record.transaction = payment.id
             else:
@@ -18,15 +22,23 @@ class MoneroSalesOrder(models.Model):
 
     # < ------Fields for Model--------------------------------------------------------------------------------------- >
 
-    is_payment_recorded = fields.Boolean('Is the Payment Recorded in this ERP', help='Cryptocurrency transactions need to be recorded and associated with this server for order handling.', default=False)
-    transaction=fields.Many2one('payment.transaction',string="Transaction", compute=_get_transaction_id)
+    is_payment_recorded = fields.Boolean(
+        "Is the Payment Recorded in this ERP",
+        help="Cryptocurrency transactions need to be recorded and associated with this server for order handling.",
+        default=False,
+    )
+    transaction = fields.Many2one(
+        "payment.transaction", string="Transaction", compute=_get_transaction_id
+    )
 
     # An order that is submitted, will have a sale order, an associated invoice, a payment, and a payment token
     # check if the payment has been completed, if so mark the payment as done
     def salesorder_payment_sync(self):
         # retrieve all the cryptocurrency payment acquirers
         # TODO search 'is_enabled' '=' True?
-        cryptocurrency_payment_acquirers = self.env['payment.acquirer'].search([('is_cryptocurrency', '=', True)])
+        cryptocurrency_payment_acquirers = self.env["payment.acquirer"].search(
+            [("is_cryptocurrency", "=", True)]
+        )
 
         for acquirer in cryptocurrency_payment_acquirers:
             pass
