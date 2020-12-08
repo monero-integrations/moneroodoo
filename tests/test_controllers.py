25451184e259d8dea2cd6df5c5ba6307d613df09
t-900-a <t-900@tutanota.com>
2020-12-07 21:55:28 -0500

    fail the unit test

--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -4,6 +4,5 @@ from ..controllers import controllers
 
 class MoneroControllerTest(unittest.TestCase):
     def test_monero_transaction(self):
-        print('dontpass')
         x=0
         assert x is 1
