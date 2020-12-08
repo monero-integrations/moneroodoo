4a2ea846ad14dcbdf9123319c47b24c777a4f18a
t-900-a <t-900@tutanota.com>
2020-12-08 00:47:49 -0500

    removed test module

--- test_payment_acquirer/tests/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import test_controllers
-from . import test_monero_acq
--- test_payment_acquirer/tests/test_monero_acq.py
+++
@@ -1,6 +0,0 @@
-import odoo.tests.common as common
-
-
-class MoneroPaymentAcquirerTest(common.TransactionCase):
-    def test_get_providers(self):
-        self.assertEquals('test', 'test')
