8d5cc981cd7360c820a056ae508956736da39776
t-900-a <t-900@tutanota.com>
2020-12-24 18:30:50 -0500

    debugging a unit tests

--- monero-rpc-odoo/models/sales_order.py
+++ monero-rpc-odoo/models/sales_order.py
@@ -8,26 +8,11 @@ _logger = logging.getLogger(__name__)
 class MoneroSalesOrder(models.Model):
     _inherit = "sale.order"
 
-    # def _get_transaction_id(self):
-    #     for record in self:
-    #         payment = self.env["payment.transaction"].search(
-    #             [("sale_order_id", "=", record.id)], limit=1
-    #         )
-    #         if payment:
-    #             record.transaction = payment.id
-    #         else:
-    #             record.transaction = False
-
     is_payment_recorded = fields.Boolean(
         "Is the Payment Recorded in this ERP",
         help="Cryptocurrency transactions need to be recorded and "
              "associated with this server for order handling.",
         default=False,
-    )
-    transaction = fields.Many2one(
-        "payment.transaction",
-        string="Transaction",
-        compute=_get_transaction_id
     )
 
     # An order that is submitted,
