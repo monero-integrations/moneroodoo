33076c5eda05ad52bf768b7a0452237840b174f7
t-900-a <t-900@tutanota.com>
2021-01-02 20:46:57 -0500

    merge 3fd73675bd3abab2b1c6325190b8687b1bc85a45

--- monero-rpc-odoo/tests/test_monero_acq.py
+++ monero-rpc-odoo/tests/test_monero_acq.py
@@ -1,6 +1,19 @@
-import odoo.tests.common as common
+from odoo.addons.payment.tests.common import PaymentAcquirerCommon
 
 
-class MoneroPaymentAcquirerTest(common.TransactionCase):
-    def test_get_providers(self):
+class MoneroCommon(PaymentAcquirerCommon):
+
+    def setUp(self):
+        super(MoneroCommon, self).setUp()
+
+        self.monero = self.env.ref('payment.payment_acquirer_monero_rpc')
+        # self.monero.write({
+        #     'is_cryptocurrency': True,
+        #     'type': 'xmr',
+        # etc...
+        # })
+
+    def test_update_rpc_server(self):
+        # this is going to fail
+        self.monero.update_rpc_server()
         self.assertEquals('test', 'test')
