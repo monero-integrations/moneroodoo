d58176fc71445a7294f428a32165ca48d8373057
t-900-a <61916790+t-900-a@users.noreply.github.com>
2025-06-29 15:40:09 -0400

    Revert "Replace legacy modules with Odoo 18 compatible payment_monero_rpc"

---
+++ monero-rpc-odoo/models/__init__.py
@@ -0,0 +1,5 @@
+from . import monero_acq, sales_order, exceptions
+
+# TODO automate prices list for currencies,
+#  the lists would be updated at a chosen interval with the correct conversion
+#  https://taiga.getmonero.org/project/t-900-monero-x-odoo-integrations/us/23
---
+++ monero-rpc-odoo/tests/test_controllers.py
@@ -0,0 +1,6 @@
+import odoo.tests.common as common
+
+
+class MoneroControllerTest(common.TransactionCase):
+    def test_monero_transaction(self):
+        self.assertEqual("test", "test")
